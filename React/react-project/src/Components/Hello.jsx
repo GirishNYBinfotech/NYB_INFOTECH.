fb993d869ae2cf9f52f8349a6ff8b19b1550341e
Girish <girish.c@nybinfotech.com>
2026-07-06 18:20:18 +0530

    react introduction

---
+++ React/react-project/src/Components/Hello.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Hello() {
+  return (
+    <h1>This is my first React APP.</h1>
+  );
+}
+
+export default Hello;
+
---
+++ React/react-project/src/Components/Newfile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Newfile = () => {
+  return (
+    <h1>this newfile of Components</h1>
+  )
+}
+
+export default Newfile
